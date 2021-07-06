aebc259e48292058641b05301a3289e69479925b
VL037 <vincent.low037@gmail.com>
2021-07-06 20:57:25 +0700

    add requestedUrl when logging in

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()){
+        req.session.requestedUrl = req.originalUrl
         req.flash('error', 'You have to login first')
         return res.redirect('/login')
     }
--- routes/users.js
+++ routes/users.js
@@ -5,28 +5,43 @@ const { isLoggedIn } = require('../middleware')
 const User = require("../models/user")
 
 router.get('/register', (req, res) => {
+    if(req.user){
+        req.flash('error', 'You have to logout first to register')
+        return res.redirect('/posts')
+    }
     res.render('users/register')
 })
 
-router.post('/register', async (req, res) => {
+router.post('/register', async (req, res, next) => {
     const { email, username, password } = req.body
     const isExists = await User.find({ email: email, username: username })
-    if (isExists) {
+    if (isExists.length > 0) {
+        console.log(await User.find({ email: email, username: username }))
         req.flash('error', 'Username / email already exists')
         return res.redirect('/register')
     }
     const newUser = new User({ email, username })
     const user = await User.register(newUser, password);
-    req.flash('success', 'Welcome to 9Gig. Remember. ~FOR FUN ONLY~')
-    res.redirect('/posts')
+    req.login(user, err => {
+        if(err) return next(err)
+        req.flash('success', 'Welcome to 9Gig. Remember. ~FOR FUN ONLY~')
+        res.redirect('/posts')
+    })
 })
 
 router.get('/login', (req, res) => {
+    if(req.user){
+        req.flash('success', 'You are already logged in')
+        return res.redirect('/posts')
+    }
     res.render('users/login')
 })
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), async (req, res) => {
-    res.redirect('/posts')
+    req.flash('success', 'Welcome back to 9Gig, have fun')
+    const redirectUrl = req.session.requestedUrl || '/posts'
+    delete req.session.requestedUrl
+    res.redirect(redirectUrl)
 })
 
 router.get('/logout', isLoggedIn, (req, res) => {
