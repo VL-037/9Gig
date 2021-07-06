5a281810a8a53275ef4c5e9f688e3198ff54a6fe
VL037 <vincent.low037@gmail.com>
2021-07-06 19:18:30 +0700

    add logout router, flash message for logout

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const { isLoggedIn } = require('../middleware')
 const User = require("../models/user")
 
 router.get('/register', (req, res) => {
@@ -25,6 +26,12 @@ router.get('/login', (req, res) => {
 })
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), async (req, res) => {
+    res.redirect('/posts')
+})
+
+router.get('/logout', isLoggedIn, (req, res) => {
+    req.logout()
+    req.flash('success', "Successfully logged out, don't forget to comeback :D")
     res.redirect('/posts')
 })
 
