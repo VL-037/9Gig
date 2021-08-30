6a4e1171a6b92b935478cefdd67ddfb02fef1bc2
VL037 <vincent.low037@gmail.com>
2021-08-30 18:19:18 +0700

    admin now can add new tag

--- controllers/tags.js
+++ controllers/tags.js
@@ -1,4 +1,5 @@
 const Tag = require('../models/tag')
+const mongoose = require('mongoose');
 
 module.exports.index = async (req, res) => {
     if(!req.user){
@@ -9,8 +10,16 @@ module.exports.index = async (req, res) => {
         return res.render('errors/404')
     } else {
         const tags = await Tag.find({}).sort({ body: 'asc' })
+        const tag_id = mongoose.Types.ObjectId();
         const currUser = req.user
-        return res.render('tags/index', { tags, currUser })
+        return res.render('tags/index', { tags, tag_id, currUser })
     }
+}
 
+module.exports.createTag = async (req, res) => {
+    var tag = new Tag(req.body.tag)
+    tag.url = req.body.tag.body.toLowerCase().replace(/\s/g, "-")
+    await tag.save()
+    req.flash('success', 'New Tag created!')
+    res.redirect('/tags')
 }
--- routes/tags.js
+++ routes/tags.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const tags = require('../controllers/tags');
 
-router.get('/', tags.index)
+router.route('/')
+    .get(tags.index)
+    .post(tags.createTag)
 
 module.exports = router
